d0c6a03546c924f21161d656fe684410239eba95
David Boedigheimer <stealth259@gmail.com>
2019-09-26 15:48:55 -0700

    Add click funtion for nameInput.

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,12 @@ $(document).ready(function() {
     var emailInput = $("input#email").val();
 
     $("ul#contact").prepend(`<li>${nameInput}</li>`);
-    // this creates and uses the variable with the nameInput. Remember "~"
+    // this creates and uses the variable with the nameInput. Remember (``)
+
+    $("nameInput").click(function() {
+      alert("this works");
+    });
+    // have almost everything. not sure where im stuck
 
     event.preventDefault();
   });
