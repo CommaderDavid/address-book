0551ffbc768c36bec68ae0a1b14e6a9a882ded19
David Boedigheimer <stealth259@gmail.com>
2019-09-26 11:55:13 -0700

    Add submit function and add contacts.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("form#book").submit(function(event) {
+    var nameInput = $("input#name").val();
+    var phoneInput = $("input#phone").val();
+    var addressInput = $("input#address").val();
+    var emailInput = $("input#email").val();
+
+    $("ul#contact").prepend(`<li>${nameInput}</li>`);
+    // this creates and uses the variable with the nameInput. Remember "~"
+
+    event.preventDefault();
+  });
+});
